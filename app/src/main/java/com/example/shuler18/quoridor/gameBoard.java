79e118780709d9b3a81667dcbf38e872faf5e596
manalili18 <manalili18@up.edu>
2018-02-06 14:22:09 -0800

    Completely compatible with both Nexus 9 and Pixel C. Need to fix remaining walls for vertical orientation.

--- app/src/main/java/com/example/shuler18/quoridor/gameBoard.java
+++ app/src/main/java/com/example/shuler18/quoridor/gameBoard.java
@@ -14,6 +14,12 @@ import android.view.SurfaceView;
 
 public class gameBoard extends SurfaceView {
 
+    private Paint brownPaint, redPaint, bluePaint, wallPaint;
+    private SurfaceView sv;
+
+    private int svHeight, svWidth, criticalSize, margin, squareSize, boardSize, startingX,
+            startingY, wallWid, wallLen;
+
 
     public gameBoard(Context context) {
         super(context);
@@ -33,32 +39,32 @@ public class gameBoard extends SurfaceView {
     public void gameBoardInit()
     {
         setWillNotDraw(false);
+
+        brownPaint = new Paint();
+        brownPaint.setColor(0xFFDEB887);
+        redPaint = new Paint();
+        redPaint.setColor(Color.RED);
+        bluePaint = new Paint();
+        bluePaint.setColor(Color.BLUE);
+        wallPaint = new Paint();
+        wallPaint.setColor(0xFF8B4513);
+
+        sv = (SurfaceView)findViewById(R.id.board);
+
+        updateGlobalMeas();
     }
 
     @Override
     public void onDraw(Canvas canvas)
     {
-        int curX, curY, margin, squareSize;
+        int curX, curY;
+
         float x0=0,y0=0,r0=0,x1=0,y1=0,r1=0;
-        SurfaceView sv = (SurfaceView)findViewById(R.id.board);
 
-        Paint brownPaint = new Paint();
-        brownPaint.setColor(0xFFDEB887);
-        Paint redPaint = new Paint();
-        redPaint.setColor(Color.RED);
-        Paint bluePaint = new Paint();
-        bluePaint.setColor(Color.BLUE);
-
-        margin = canvas.getHeight() / 9 - 10;
-        squareSize = margin * 2 / 3;
-
-
-        int startingX = canvas.getWidth() - margin * 10;
-        int startingY = canvas.getHeight() - margin * 10 + squareSize;
+        updateGlobalMeas();
 
         curX = startingX;
         curY = startingY;
-
 
         //draws board
         for(int i = 0; i < 9; i++)
@@ -68,7 +74,8 @@ public class gameBoard extends SurfaceView {
             {
                 canvas.drawRect(curX, curY, curX+squareSize, curY+squareSize, brownPaint);
                 curX += margin;
-                
+
+                //draw player1
                 if(j == 4 && i == 1)
                 {
                     x0 = curX+(squareSize*.5f);
@@ -76,12 +83,36 @@ public class gameBoard extends SurfaceView {
                     r0 = squareSize*.45f;
                 }
 
+                //draw player2
                 if(j == 6 && i == 6)
                 {
                     x1 = curX+(squareSize*.5f);
                     y1 = curY+(squareSize*.5f);
                     r1 = squareSize*.45f;
                 }
+
+                //draw horz wall
+                if(i == 2 && j == 3)
+                {
+                    canvas.drawRect(
+                            curX,
+                            curY-(wallWid+(margin-squareSize))/2,
+                            curX+squareSize+margin,
+                            curY-((margin-squareSize)-(wallWid+(margin-squareSize))/2),
+                            wallPaint);
+                }
+
+                //draw vert wall
+                if(i == 1 && j == 5)
+                {
+                    canvas.drawRect(
+                            curX -(wallWid+(margin-squareSize))/2,
+                            curY,
+                            curX-((margin-squareSize)-(wallWid+(margin-squareSize))/2),
+                            curY+squareSize+margin,
+                            wallPaint);
+                }
+
 
             }
             curX = startingX;
@@ -93,6 +124,57 @@ public class gameBoard extends SurfaceView {
         canvas.drawCircle(x0,y0,r0, redPaint);
         canvas.drawCircle(x1,y1,r1, bluePaint);
 
+        drawP1RemainingWalls(canvas);
+        drawP2RemainingWalls(canvas);
+
+    }
+
+    void updateGlobalMeas()
+    {
+        svHeight = sv.getHeight();
+        svWidth = sv.getWidth();
+        criticalSize = (svHeight > svWidth) ? svWidth : svHeight;
+        margin = criticalSize / 9 - 10;
+        squareSize = margin * 2 / 3;
+
+        boardSize = margin * 9 - (margin - squareSize);
+        startingX = (svWidth - boardSize)/2;
+        startingY = (svHeight - boardSize)/2;
+        wallLen = margin + squareSize;
+        wallWid = ( margin - squareSize ) / 2;
+    }
+
+    void drawP1RemainingWalls(Canvas canvas)
+    {
+        int curX, curY;
+        curX = startingX - (margin - squareSize);
+        curY = startingY;
+
+        for (int i=0; i<10; i++)
+        {
+            canvas.drawRect(curX-wallLen,
+                    curY+2*i*wallWid,
+                    curX,
+                    curY+wallWid+2*i*wallWid,
+                    wallPaint);
+        }
+    }
+
+    void drawP2RemainingWalls(Canvas canvas)
+    {
+        int curX, curY;
+        curX = startingX + (margin*9) ;
+        curY = startingY + (margin*8 + squareSize);
+
+        for (int i=0; i<8; i++)
+        {
+            canvas.drawRect(curX,
+                    curY,
+                    curX + wallLen,
+                    curY-wallWid,
+                    wallPaint);
+            curY -= 2 * wallWid;
+        }
     }
 
 
